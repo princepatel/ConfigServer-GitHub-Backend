f02011992d4e9a4dc2a7dca8dc988057897e947f
Prashant Patel <ppatel@gmail.com>
2020-08-17 13:16:24 +0530

    First commit

---
+++ src/main/java/com/configserver/github/ConfigServerGitHubBackendApplication.java
@@ -0,0 +1,13 @@
+package com.configserver.github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerGitHubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerGitHubBackendApplication.class, args);
+	}
+
+}
